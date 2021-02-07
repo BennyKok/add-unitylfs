3e5379d3933b9b7698bb3e4c6b42fbfdda796cc8
BennyKok <itechbenny@gmail.com>
2021-02-07 23:21:16 +0800

    fix: unable to run correctly

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-var fs = require('fs')
-var path = require('path')
+#! /usr/bin/env node
 
-fs.copyFile(path.join('.', "resources", '.gitattributes'), path.join(process.cwd(), '.gitattributes'), (err) => {
+const fs = require('fs')
+const path = require('path')
+const { exec } = require("child_process");
+
+// copy attr file
+fs.copyFile(path.join(__dirname, "resources", '.gitattributes'), path.join(process.cwd(), '.gitattributes'), (err) => {
     if (err)
         throw err;
     console.log('.gitattributes was Added!');
+
+    // setup lfs
+
+    if (fs.existsSync(path.join(process.cwd(), '.git'))) {
+        exec("git lfs install", (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
+    } else {
+        console.log('Not a git repo here! Just a reminder...');
+    }
 })
